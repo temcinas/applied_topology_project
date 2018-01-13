0752cacbe85629325be497f61ced79445e608758
Unknown <temcinas@gmail.com>
2018-01-13 16:24:53 +0000

    finished vertex worker

--- workers.py
+++ workers.py
@@ -1,47 +1,47 @@
-from complex import DataComplex
+from complex import VietorisRipsComplex
 
 
-class SimplexWorker:
-    # Class to calculate local homology of a simplex (I will use only for edges and vertices)
+class VertexWorker:
+    # Class to calculate local homology of a vertex and edges coming from it
     workers = []
     epsilon = 0
     space_dimension = 0
     vertex_homologies = {}
+    edge_homologies = {}
 
     @classmethod
     def set_params(cls, *, epsilon, dimension):
         cls.epsilon = epsilon
         cls.space_dimension = dimension
 
-    def __init__(self, simplex, distance_matrix, *, simplex_dim, neighbours=None):
-        # Simplex - either vertex_id or a 2-element set of vertex ids
+    def __init__(self, vertex_id, distance_matrix, neighbours):
         # Distance_matrix - matrix of pairwise distances
-        # Neighbours - ids of neighbours, if applicable, 0th position is the vertex itself
+        # Neighbours - ids of neighbours, 0th position is the vertex itself
 
-        if not SimplexWorker.epsilon or not SimplexWorker.space_dimension:
+        if not VertexWorker.epsilon or not VertexWorker.space_dimension:
             raise ValueError('both epsilon and dimension have to be non-zero, please set the params!')
 
-        if simplex in SimplexWorker.workers:
-            raise ValueError("worker for vertex {0} already created!".format(simplex))
-        SimplexWorker.workers.append(simplex)
+        if vertex_id in VertexWorker.workers:
+            raise ValueError("worker for vertex {0} already created!".format(vertex_id))
+        VertexWorker.workers.append(vertex_id)
 
         self.distance_matrix = distance_matrix
-        self.simplex = simplex
+        self.vertex_id = vertex_id
         self.active = False
-        # 0-simplex is vertex; 1-simplex is edge
-        self.smp_dim = simplex_dim
-        if neighbours:
-            self.neighbours = neighbours
+        self.neighbours = neighbours[1:]
 
     def start_calculation(self):
         # potentially the worker could create a thread and here is where parallelism would start
         self.active = True
-        data_complex = DataComplex(distance_matrix=self.distance_matrix,
-                                   dim=SimplexWorker.space_dimension,
-                                   epsilon=SimplexWorker.epsilon)
+        data_complex = VietorisRipsComplex(distance_matrix=self.distance_matrix,
+                                           dim=VertexWorker.space_dimension,
+                                           epsilon=VertexWorker.epsilon)
         data_complex.build_vr_complex()
-        # TODO: I ended here
-        simplex = set(range(self.smp_dim + 1))
-        betti_numbers = data_complex.get_localhom(simplex)
-        SimplexWorker.vertex_homologies[self.simplex] = betti_numbers
+        vertex_homology = data_complex.get_localhom({0})
+        VertexWorker.vertex_homologies[self.vertex_id] = vertex_homology
+        for neighbour_index, neighbour in enumerate(self.neighbours, 1):
+            edge = frozenset({self.vertex_id, neighbour})
+            if edge not in VertexWorker.edge_homologies.keys():
+                edge_homology = data_complex.get_localhom({0, neighbour_index})
+                VertexWorker.edge_homologies[edge] = edge_homology
         self.active = False
